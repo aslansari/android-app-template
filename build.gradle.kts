bf89904791fe8b13565ae3167f63f8cbdc1fded4
Aslan Sarı <aslansari.dev@gmail.com>
2023-05-15 19:21:47 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+
+    dependencies {
+        classpath(libs.detekt.gradle.plugin)
+        classpath(libs.gradle)
+        classpath(libs.gradle.versions.plugin)
+        classpath(libs.kotlin.gradle.plugin)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+plugins {
+    id("com.google.dagger.hilt.android").version(libs.versions.hilt).apply(false)
+    id("io.gitlab.arturbosch.detekt").version(libs.versions.detektGradlePlugin)
+    id("org.jmailen.kotlinter").version(libs.versions.kotlinter).apply(false)
+}
+
+apply(from = "buildscripts/githooks.gradle")
+apply(from = "buildscripts/setup.gradle")
+apply(from = "buildscripts/versionsplugin.gradle")
+
+subprojects {
+    apply(from = "../buildscripts/detekt.gradle")
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+afterEvaluate {
+    // We install the hook at the first occasion
+    tasks.named("clean") {
+        dependsOn(":installGitHooks")
+    }
+}
+
+tasks {
+    /**
+     * The detektAll tasks enables parallel usage for detekt so if this project
+     * expands to multi module support, detekt can continue to run quickly.
+     *
+     * https://proandroiddev.com/how-to-use-detekt-in-a-multi-module-android-project-6781937fbef2
+     */
+    @Suppress("UnusedPrivateMember")
+    val detektAll by registering(io.gitlab.arturbosch.detekt.Detekt::class) {
+        parallel = true
+        setSource(files(projectDir))
+        include("**/*.kt")
+        include("**/*.kts")
+        exclude("**/resources/**")
+        exclude("**/build/**")
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = false
+    }
+}
