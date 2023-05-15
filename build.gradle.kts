bac2ac9d1c98580747ddd6a35a08f7352e520022
Aslan Sarı <aslansari.dev@gmail.com>
2023-05-15 23:39:17 +0300

    chore: convert old classpath plugins to kts plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-
-    repositories {
-        google()
-        mavenCentral()
-        maven(url = "https://plugins.gradle.org/m2/")
-    }
-
-    dependencies {
-        classpath(libs.detekt.gradle.plugin)
-        classpath(libs.gradle)
-        classpath(libs.gradle.versions.plugin)
-        classpath(libs.kotlin.gradle.plugin)
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
 plugins {
-    id("com.google.dagger.hilt.android").version(libs.versions.hilt).apply(false)
-    id("io.gitlab.arturbosch.detekt").version(libs.versions.detektGradlePlugin)
-    id("org.jmailen.kotlinter").version(libs.versions.kotlinter).apply(false)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.gradle.versions)
+    alias(libs.plugins.kotlinter) apply false
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.com.android.test) apply false
 }
 
 apply(from = "buildscripts/githooks.gradle")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
