5b914d05aed2854247669bc806d9c15adb0feff2
Aslan <aslansari.dev@gmail.com>
2024-03-21 23:14:06 +0300

    feat: configure flavor and app name

--- build-logic/convention/src/main/kotlin/template/Flavor.kt
+++ build-logic/convention/src/main/kotlin/template/Flavor.kt
@@ -5,25 +5,28 @@ import com.android.build.api.dsl.ApplicationProductFlavor
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 
+@Suppress("EnumEntryName", "EnumNaming")
 enum class FlavorDimension {
-    CONTENT_TYPE
+    contentType
 }
 
 // The content for the app can either come from local static data which is useful for demo
 // purposes, or from a production backend server which supplies up-to-date, real content.
 // These two product flavors reflect this behaviour.
-enum class Flavor (val dimension : FlavorDimension, val applicationIdSuffix : String? = null) {
-    DEMO(FlavorDimension.CONTENT_TYPE),
-    PROD(FlavorDimension.CONTENT_TYPE, ".prod")
+@Suppress("EnumEntryName", "EnumNaming")
+enum class Flavor(val dimension: FlavorDimension, val applicationIdSuffix: String? = null) {
+    dev(FlavorDimension.contentType, ".dev"),
+    beta(FlavorDimension.contentType, ".beta"),
+    prod(FlavorDimension.contentType)
 }
 
 fun Project.configureFlavors(
-    commonExtension: CommonExtension<*, *, *, *, *>
+    commonExtension: CommonExtension<*, *, *, *, *>,
 ) {
     commonExtension.apply {
-        flavorDimensions += FlavorDimension.CONTENT_TYPE.name
+        flavorDimensions += FlavorDimension.contentType.name
         productFlavors {
-            Flavor.values().forEach{
+            Flavor.values().forEach {
                 create(it.name) {
                     dimension = it.dimension.name
                     if (this@apply is ApplicationExtension && this is ApplicationProductFlavor) {
@@ -31,6 +34,14 @@ fun Project.configureFlavors(
                             this.applicationIdSuffix = it.applicationIdSuffix
                         }
                     }
+                    val appName = buildString {
+                        append("template")
+                        if (it.applicationIdSuffix != null) {
+                            append(" - ")
+                            append(it.applicationIdSuffix.uppercase())
+                        }
+                    }
+                    resValue("string", "app_name", appName)
                 }
             }
         }
